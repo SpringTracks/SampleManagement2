1f8b1b0469106ce7cb6e1289042c7fd60c60c61c
xiangchun.zhang <xiangchun.zhang@lge.com>
2016-09-09 17:56:34 +0900

    Add EmployeeID search

--- SampleManagement2/src/com/lge/strcture/EmployeeAdapter.java
+++ SampleManagement2/src/com/lge/strcture/EmployeeAdapter.java
@@ -59,7 +59,7 @@ public class EmployeeAdapter extends CursorAdapter implements Filterable {
 	        if(mDBManager==null){
 	        	Log.i("ModelAdapter","dbmanager is null");
 	        }
-	        mCursor = mDBManager.queryDataByKey(DBOpenHandler.EMPLOYEE_TABLE_NAME,DBOpenHandler.EMPLOYEE_TABLE_KEY[0],constraint.toString());
+	        mCursor = mDBManager.queryVagueDataByKey(DBOpenHandler.EMPLOYEE_TABLE_NAME,DBOpenHandler.EMPLOYEE_TABLE_KEY[0],constraint.toString());
 	       // getDBManager().closeDataBase();
 	        return mCursor;
 	    }
